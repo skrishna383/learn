b980d7a517d5f2887f326ff848cbcfaabac8b3a5
skrishna383 <skrishna383@gmail.com>
2019-07-25 12:09:32 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,5 +3,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello sai!");
         System.out.println(" my first commit");
+        System.out.println("enter your name");
+        
 
 }}
