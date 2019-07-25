3bcd2909d5b35a9b16c4ee165a8cb4d6a97fe343
skrishna383 <skrishna383@gmail.com>
2019-07-25 12:04:02 +0530

    who are you

--- Hello.java
+++ Hello.java
@@ -3,5 +3,11 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello sai!");
         System.out.println(" my first commit");
+        
+        
+
+        Scanner sai = new Scanner(System.in);
+        String name = sai.nextLine();
+        System.out.println("hello "+name);
 
 }}
